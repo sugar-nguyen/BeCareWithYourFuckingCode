4d6c82ee6ad060ec5d2b235ec83de55b14251b4b
Linh Đường <44584336+sugar-nguyen@users.noreply.github.com>
2019-03-08 16:14:25 +0700

    16:14

--- BeCareWithYourFuckingCode/Controllers/UserController.cs
+++ BeCareWithYourFuckingCode/Controllers/UserController.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using BeCareWithYourFuckingCode.Models;
 namespace BeCareWithYourFuckingCode.Controllers
 {
     public class UserController : Controller
     {
         //
         // GET: /User/
+
+        WEBACCOUNTEntities2 Entities = new WEBACCOUNTEntities2();
         public ActionResult Index()
         {
             return View();
@@ -20,8 +22,18 @@ namespace BeCareWithYourFuckingCode.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-        public ActionResult getSingUp()
+        public ActionResult getSignUp()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult getSignUp(TB_USER user)
+        {
+            if (ModelState.IsValid)
+            {
+                Entities.TB_USER.Add(user);
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
 	}
